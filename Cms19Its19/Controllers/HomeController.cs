919ea2fc9e830ca63b114677f2bf8b339aaa45a2
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-03-04 12:15:37 +0100

    Add project files.

---
+++ Cms19Its19/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Cms19Its19.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Cms19Its19.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult ListAccounts()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult SeeTransactions(string id)
+        {
+            return View(model:id);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
